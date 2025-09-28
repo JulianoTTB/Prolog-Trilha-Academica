770c8083a107093f32d8153bf4a2a13a1ff9854b
GeandroMarques <166722959+GeandroMarques@users.noreply.github.com>
2025-09-28 17:46:59 -0300

    perfil_teste_2.pl
    Perfil 2: Aluno com inclinação para arquitetura de computadores.

---
+++ perfil_teste_2.pl
@@ -0,0 +1,15 @@
+% Perfil: Aluno com forte inclinação para arquitetura de computadores.
+
+pergunta(1, n). % Resposta para pergunta 1
+pergunta(2, s). % Resposta para pergunta 2
+pergunta(3, n). % Resposta para pergunta 3
+pergunta(4, s). % Resposta para pergunta 4
+pergunta(5, s). % Resposta para pergunta 5
+pergunta(6, n). % Resposta para pergunta 6
+pergunta(7, s). % Resposta para pergunta 7
+pergunta(8, s). % Resposta para pergunta 8
+pergunta(9, s). % Resposta para pergunta 9
+pergunta(10, s). % Resposta para pergunta 10
+pergunta(11, n). % Resposta para pergunta 11
+pergunta(12, n). % Resposta para pergunta 12
+pergunta(13, n). % Resposta para pergunta 13
