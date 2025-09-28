dc7e8c9f0dd89dfe00d27f0fe5f6fe11be9bc658
GeandroMarques <166722959+GeandroMarques@users.noreply.github.com>
2025-09-28 16:32:08 -0300

    Update base_conhecimento.pl

--- base_conhecimento.pl
+++ base_conhecimento.pl
@@ -13,30 +13,77 @@ perfil(ciberseguranca, pensamento_critico, 3).
 perfil(ciberseguranca, criptografia, 5).
 perfil(ciberseguranca, programacao, 5).
 perfil(ciberseguranca, software, 5).
+perfil(ciberseguranca, machine_learning, 3).
+perfil(ciberseguranca, matematica_e_estatistica, 4).
+perfil(ciberseguranca, eletronica, 5).
+perfil(ciberseguranca, hardware, 5).
+perfil(ciberseguranca, matematica_discreta, 5).
+perfil(ciberseguranca, otimizacao_sistemas, 3).
+perfil(ciberseguranca, analise_dados, 5).
+perfil(ciberseguranca, padroes_tendencias, 5).
 
 % Inteligência artificial
 perfil(inteligencia_artificial, machine_learning, 5).
 perfil(inteligencia_artificial, matematica_e_estatistica, 4).
 perfil(inteligencia_artificial, programacao, 5).
 perfil(inteligencia_artificial, software, 5).
+perfil(inteligencia_artificial, sistemas_e_redes, 5).
+perfil(inteligencia_artificial, pensamento_critico, 4).
+perfil(inteligencia_artificial, criptografia, 5).
+perfil(inteligencia_artificial, eletronica, 5).
+perfil(inteligencia_artificial, hardware, 5).
+perfil(inteligencia_artificial, matematica_discreta, 4).
+perfil(inteligencia_artificial, otimizacao_sistemas, 5).
+perfil(inteligencia_artificial, analise_dados, 5).
+perfil(inteligencia_artificial, padroes_tendencias, 5).
 
 % IoT
 perfil(internet_das_coisas, hardware, 3).
 perfil(internet_das_coisas, software, 3).
 perfil(internet_das_coisas, eletronica, 5).
+perfil(internet_das_coisas, machine_learning, 3).
+perfil(internet_das_coisas, matematica_e_estatistica, 3).
+perfil(internet_das_coisas, programacao, 5).
+perfil(internet_das_coisas, sistemas_e_redes, 3).
+perfil(internet_das_coisas, pensamento_critico, 3).
+perfil(internet_das_coisas, criptografia, 5).
+perfil(internet_das_coisas, matematica_discreta, 3).
+perfil(internet_das_coisas, otimizacao_sistemas, 5).
+perfil(internet_das_coisas, analise_dados, 3).
+perfil(internet_das_coisas, padroes_tendencias, 3).
 
 % Arquitetura de computadores
 perfil(arquitetura_de_computadores, hardware, 5).
 perfil(arquitetura_de_computadores, matematica_discreta, 4).
 perfil(arquitetura_de_computadores, otimizacao_sistemas, 3).
 perfil(arquitetura_de_computadores, eletronica, 5).
+perfil(arquitetura_de_computadores, software, 5).
+perfil(arquitetura_de_computadores, machine_learning, 4).
+perfil(arquitetura_de_computadores, matematica_e_estatistica, 3).
+perfil(arquitetura_de_computadores, programacao, 5).
+perfil(arquitetura_de_computadores, sistemas_e_redes, 5).
+perfil(arquitetura_de_computadores, pensamento_critico, 4).
+perfil(arquitetura_de_computadores, criptografia, 3).
+perfil(arquitetura_de_computadores, matematica_discreta, 5).
+perfil(arquitetura_de_computadores, otimizacao_sistemas, 5).
+perfil(arquitetura_de_computadores, analise_dados, 4).
+perfil(arquitetura_de_computadores, padroes_tendencias, 3).
+
 
 % Ciência de dados
 perfil(ciencia_de_dados, matematica_e_estatistica, 5).
 perfil(ciencia_de_dados, analise_de_dados, 5).
 perfil(ciencia_de_dados, padroes_e_tendencias, 5).
 perfil(ciencia_de_dados, software, 4).
-
+perfil(ciencia_de_dados, hardware, 5).
+perfil(ciencia_de_dados, matematica_discreta, 5).
+perfil(ciencia_de_dados, otimizacao_sistemas, 5).
+perfil(ciencia_de_dados, eletronica, 4).
+perfil(ciencia_de_dados, machine_learning, 5).
+perfil(ciencia_de_dados, programacao, 5).
+perfil(ciencia_de_dados, sistemas_e_redes, 5).
+perfil(ciencia_de_dados, pensamento_critico, 4).
+perfil(ciencia_de_dados, criptografia, 5).
 
 % Perguntas
 pergunta(1, "Você tem afinidade com matemática e estatística?", matematica_e_estatistica).
