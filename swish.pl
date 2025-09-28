cc7f42f29a9408db4e2bcacaca7710bb3eaf8d0c
GeandroMarques <166722959+GeandroMarques@users.noreply.github.com>
2025-09-28 14:55:08 -0300

    Trilhas e perfis

---
+++ swish.pl
@@ -0,0 +1,37 @@
+% Trilhas de Especialização
+trilha(ciberseguranca, 'Proteção de dados e sistemas contra ameaças digitais').
+trilha(inteligencia_artificial, 'Criação de sistemas inteligentes capazes de aprender e tomar decisões').
+trilha(internet_das_coisas, 'Integração de dispositivos físicos conectados à internet para automação e monitoramento').
+trilha(arquitetura_de_computadores, 'Estudo da organização, funcionamento e otimização do hardware e sistemas computacionais').
+trilha(ciencia_de_dados, 'Coleta, análise e interpretação de grandes volumes de dados').
+
+% Características de perfil
+% Cibersegurança
+perfil(ciberseguranca, sistemas_e_redes, 4).
+perfil(ciberseguranca, pensamento_critico, 3).
+perfil(ciberseguranca, criptografia, 5).
+perfil(ciberseguranca, programacao, 5).
+perfil(ciberseguranca, software, 5).
+
+% Inteligência artificial
+perfil(inteligencia_artificial, machine_learning, 5).
+perfil(inteligencia_artificial, matematica_e_estatistica, 4).
+perfil(inteligencia_artificial, programacao, 5).
+perfil(inteligencia_artificial, software, 5).
+
+% IoT
+perfil(internet_das_coisas, hardware, 3).
+perfil(internet_das_coisas, software, 3).
+perfil(internet_das_coisas, eletronica, 5).
+
+% Arquitetura de computadores
+perfil(arquitetura_de_computadores, hardware, 5).
+perfil(arquitetura_de_computadores, matematica_discreta, 4).
+perfil(arquitetura_de_computadores, otimizacao_sistemas, 3).
+perfil(arquitetura_de_computadores, eletronica, 5).
+
+% Ciência de dados
+perfil(ciencia_de_dados, matematica_e_estatistica, 5).
+perfil(ciencia_de_dados, analise_de_dados, 5).
+perfil(ciencia_de_dados, padroes_e_tendencias, 5).
+perfil(ciencia_de_dados, software, 4).
