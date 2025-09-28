8b4ebdbb98eecafb48dea5d13a144413bb1a6aca
Alissinnho <166721852+Alissinnho@users.noreply.github.com>
2025-09-28 19:29:29 -0300

    Update and rename perfil_teste_1.pl to perfil_1.pl

---
+++ perfil_1.pl
@@ -0,0 +1,15 @@
+% Perfil: Aluno com forte inclinação para inteligência artificial.
+
+resposta(1, s). % Resposta para pergunta 1
+resposta(2, n). % Resposta para pergunta 2
+resposta(3, s). % Resposta para pergunta 3
+resposta(4, n). % Resposta para pergunta 4
+resposta(5, s). % Resposta para pergunta 5
+resposta(6, s). % Resposta para pergunta 6
+resposta(7, s). % Resposta para pergunta 7
+resposta(8, s). % Resposta para pergunta 8
+resposta(9, n). % Resposta para pergunta 9
+resposta(10, n). % Resposta para pergunta 10
+resposta(11, s). % Resposta para pergunta 11
+resposta(12, n). % Resposta para pergunta 12
+resposta(13, s). % Resposta para pergunta 13
