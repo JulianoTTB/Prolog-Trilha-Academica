4cac24334f5a32fe7a424f8d20b72c1ab56be74d
GeandroMarques <166722959+GeandroMarques@users.noreply.github.com>
2025-09-28 21:53:43 -0300

    perfil_2.pl

--- perfil_2.pl
+++ perfil_2.pl
@@ -1,15 +1,15 @@
 % Perfil: Aluno com forte inclinação para arquitetura de computadores.
 
-resposta(1, n). % Resposta para pergunta 1
-resposta(2, s). % Resposta para pergunta 2
-resposta(3, n). % Resposta para pergunta 3
-resposta(4, s). % Resposta para pergunta 4
-resposta(5, s). % Resposta para pergunta 5
-resposta(6, n). % Resposta para pergunta 6
-resposta(7, s). % Resposta para pergunta 7
-resposta(8, s). % Resposta para pergunta 8
-resposta(9, s). % Resposta para pergunta 9
-resposta(10, s). % Resposta para pergunta 10
-resposta(11, n). % Resposta para pergunta 11
-resposta(12, n). % Resposta para pergunta 12
-resposta(13, n). % Resposta para pergunta 13
+resposta(1, "n"). % Resposta para pergunta 1
+resposta(2, "s"). % Resposta para pergunta 2
+resposta(3, "n"). % Resposta para pergunta 3
+resposta(4, "s"). % Resposta para pergunta 4
+resposta(5, "s"). % Resposta para pergunta 5
+resposta(6, "n"). % Resposta para pergunta 6
+resposta(7, "s"). % Resposta para pergunta 7
+resposta(8, "s"). % Resposta para pergunta 8
+resposta(9, "s"). % Resposta para pergunta 9
+resposta(10, "s"). % Resposta para pergunta 10
+resposta(11, "n"). % Resposta para pergunta 11
+resposta(12, "n"). % Resposta para pergunta 12
+resposta(13, "n"). % Resposta para pergunta 13
